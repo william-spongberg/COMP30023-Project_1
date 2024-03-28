1c8fae0b2233c4ebfbbbab0d1a7536bbf7aaafdf
Liang(Lewis) Zhang <liang.lewis.z@gmail.com>
2024-03-28 11:35:08 +1100

    added process module for managing process structures and functions for updating processes

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Global variable for keeping track of simulation time and cycle
+int sim_time = 0;
+int cycle;
+
+// Global enum to store the 3 possible quanta allowed
+enum Quanta {
+    QUANTA_1 = 1,
+    QUANTA_2 = 2,
+    QUANTA_3 = 3
+};
+
 int main() {
+    
     return 0;
 }
