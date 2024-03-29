b99ff276f6d1852c7abf642d7b4d1c435ae34be4
Liang(Lewis) Zhang <liang.lewis.z@gmail.com>
2024-03-29 16:14:42 +1100

    implemented the queue module for storing processes, will need to be refactored when new needs discovered the solution develops

---
+++ mem.c
@@ -0,0 +1,31 @@
+#include "mem.h"
+
+// Define helper functions for each memory strategy
+// FIXME: Add appropriate arguments to each function
+void infinite();
+void first_fit();
+void paged();
+void virtual();
+
+// Implement memory allocation function
+// FIXME: Add appropriate arguments to function signature
+void allocate_memory(int size, memory_strategy_t strategy) {
+    switch (strategy) {
+        case INFINITE:
+            infinite();
+            break;
+        case FIRST_FIT:
+            first_fit();
+            break;
+        case PAGED:
+            paged();
+            break;
+        case VIRTUAL:
+            virtual();
+            break;
+    }
+    return NULL;
+}
+
+// TODO: Implement individual memory strategy functions
+
---
+++ mem.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// define enum of memory strategies
+typedef enum {
+    INFINITE,
+    FIRST_FIT,
+    PAGED,
+    VIRTUAL
+} memory_strategy_t;
+
+// define wrapper memory functions for each strategy
+// FIXME: Determine whether return process or take process as argument and allocate memory accordingly
+void allocate_memory(int size, memory_strategy_t strategy);
