141f546aaaf824775243b32f9cc2f8ab894df886
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-04-01 19:35:55 +1100

    delegated running simulation to process_manager

--- src/main.c
+++ src/main.c
@@ -1,16 +1,12 @@
-#include "round_robin.h"
+#include "process_manager.h"
 #include <getopt.h>
 #include <unistd.h>
-
-Process **load_processes(char *filename, int *n);
-void set_memory_strategy(char *memory_strategy);
 
 int main(int argc, char *argv[]) {
     char *filename = NULL;
     char *memory_strategy = NULL;
     int quantum = 0;
     int opt;
-    int n = 0;
 
     // parse command line arguments
     while ((opt = getopt(argc, argv, "f:m:q:")) != -1) {
@@ -39,61 +35,8 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // load processes and memory strategy from file
-    Process **processes = load_processes(filename, &n);
-    set_memory_strategy(memory_strategy);
-
-    // do round robin scheduling
-    round_robin_scheduling(processes, n, quantum);
+    // simulation
+    run_simulation(filename, memory_strategy, quantum);
 
     return 0;
-}
-
-Process **load_processes(char *filename, int *n) {
-    // open file
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // unknown number of processes
-    Process **processes = (Process **)malloc(sizeof(Process *));
-    assert(processes != NULL);
-
-    // format: time process-name remaining-time memory
-    // e.g. 0 P1 10 100
-    char name[8];
-    int time, rtime;
-    int mem = 0;
-
-    // read file
-    while (fscanf(file, "%d %s %d %d", &time, name, &rtime, &mem) != EOF) {
-        processes =
-            (Process **)realloc(processes, (*n + 1) * sizeof(Process *));
-        assert(processes != NULL);
-        // TODO: set to BLOCKED since haven't arrived in CPU yet?
-        // initial state of READY
-        processes[*n] = create_process(time, name, rtime, mem, READY);
-        *n += 1;
-    }
-    fclose(file);
-
-    return processes;
-}
-
-// TODO: set memory strategies
-void set_memory_strategy(char *memory_strategy) {
-    if (strcmp(memory_strategy, "infinite") == 0) {
-        // set strategy
-    } else if (strcmp(memory_strategy, "first-fit") == 0) {
-        // set strategy
-    } else if (strcmp(memory_strategy, "paged") == 0) {
-        // set strategy
-    } else if (strcmp(memory_strategy, "virtual") == 0) {
-        // set strategy
-    } else {
-        fprintf(stderr, "Invalid memory strategy\n");
-        exit(EXIT_FAILURE);
-    }
 }
