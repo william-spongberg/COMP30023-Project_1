2172b7e9d27e5f9c3785077898aa37401396ac30
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-04-02 18:58:40 +1100

    removed unnecessary header

--- src/process_scheduler.h
+++ src/process_scheduler.h
@@ -1,7 +1,6 @@
 #ifndef PROCESS_SCHEDULER_H
 #define PROCESS_SCHEDULER_H
 #include "memory_manager.h"
-#include "queue.h"
 
 // method signatures
 void round_robin_scheduling(Node **queue, Process **p, p_state *curr_state);
