ee7d5a856de8c64ee39f3b4a6caf01fe6810fc78
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-04-02 14:34:45 +1100

    fixed first-fit, added process memory print

--- src/memory_manager.c
+++ src/memory_manager.c
@@ -1,57 +1,5 @@
 #include "memory_manager.h"
 
-// Create block of memory
-memory_t *create_memory() {
-    memory_t *mem = (memory_t *)malloc(sizeof(memory_t));
-    mem->size = MEM_SIZE;
-    mem->available = MEM_SIZE;
-    mem->blocks = (int8_t *)calloc(MEM_SIZE, sizeof(int8_t));
-    return mem;
-}
-
-// Implement first fit strategy
-
-/*
-    Helper:
-    Return the index of the first bit in the memory block that
-    can fit the required size
-*/
-int first_available(memory_t *mem, int size) {
-    int count = 0;
-    int start = 0;
-    for (int i = 0; i < mem->size; i++) {
-        if (mem->blocks[i] == 0) {
-            if (start == -1) {
-                start = i;
-            }
-            count++;
-        } else {
-            count = 0;
-            start = -1;
-        }
-
-        if (count == size) {
-            return start;
-        }
-    }
-    return -1;
-}
-
-/* Actual first fit function */
-int8_t first_fit(memory_t *mem, int size) {
-    int start = first_available(mem, size);
-    if (start == -1) {
-        return -1;
-    }
-
-    for (int i = start; i < start + size; i++) {
-        mem->blocks[i] = 1;
-    }
-    mem->available -= size;
-
-    return start;
-}
-
 // TODO: set memory strategies
 void set_memory_strategy(char *memory_strategy) {
     if (strcmp(memory_strategy, "infinite") == 0) {
@@ -68,16 +16,86 @@ void set_memory_strategy(char *memory_strategy) {
     }
 }
 
-// Free memory
-void free_memory(memory_t *mem, int start, int size) {
-    for (int i = start; i < start + size; i++) {
-        mem->blocks[i] = 0;
+// create block of memory
+memory_t *create_memory(char *memory_strategy) {
+    if (strcmp(memory_strategy, "infinite") == 0) {
+        return NULL;
+    } else if (strcmp(memory_strategy, "first-fit") == 0) {
+        memory_t *mem = (memory_t *)malloc(sizeof(memory_t));
+        mem->size = MEM_SIZE;
+        mem->available = MEM_SIZE;
+        for (int i = 0; i < MEM_SIZE; i++) {
+            mem->blocks[i] = 0;
+        }
+        return mem;
     }
-    mem->available += size;
+    return NULL;
 }
 
-// Clean up
+// allocate memory
+int16_t allocate_memory(memory_t *mem, char *memory_strategy, int size) {
+    if (strcmp(memory_strategy, "infinite") == 0) {
+        return -1;
+    } else if (strcmp(memory_strategy, "first-fit") == 0) {
+        return first_fit(mem, size);
+    }
+    fprintf(stderr, "Invalid memory strategy\n");
+    exit(EXIT_FAILURE);
+}
+
+// implement first fit strategy
+int16_t first_fit(memory_t *mem, int size) {
+    int start = -1;
+    int count = 0;
+    for (int i = 0; i < mem->size; i++) {
+        if (mem->blocks[i] == 0) {
+            if (start == -1) {
+                start = i;
+            }
+            count++;
+        } else {
+            start = -1;
+            count = 0;
+        }
+        if (count == size) {
+            for (int j = start; j < start + size; j++) {
+                mem->blocks[j] = 1;
+            }
+            mem->available -= size;
+            return start;
+        }
+    }
+    return -1;
+}
+
+// free memory
+void free_memory(memory_t *mem, int start, int size) {
+    if (mem != NULL) {
+        for (int i = start; i < start + size; i++) {
+            mem->blocks[i] = 0;
+        }
+        mem->available += size;
+    }
+}
+
+// clean up
 void destroy_memory(memory_t *mem) {
-    free(mem->blocks);
-    free(mem);
+    if (mem != NULL)
+        free(mem);
+}
+
+// print memory usage
+int get_mem_usage(memory_t *mem) {
+    return ceil(100 - (((float)mem->available / (float)MEM_SIZE) * 100));
+}
+
+void print_memory_process(Process *p, memory_t *mem, int sim_time) {
+    if (mem == NULL) {
+        fprintf(stderr, "mem is NULL\n");
+        exit(EXIT_FAILURE);
+    }
+    printf(
+        "%d,%s,process-name=%s,remaining-time=%d,mem-usage=%d%%,allocated-at=%"
+        "d\n",
+        sim_time, get_state(p), p->name, p->rtime, get_mem_usage(mem), p->addr);
 }
--- src/memory_manager.h
+++ src/memory_manager.h
@@ -1,34 +1,30 @@
 #ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
 #include "process.h"
-#include <stdint.h>
+#include <math.h>
 
 #define MEM_SIZE 2048 // KB
 
 typedef struct memory {
     int size;
     int available;
-    // int8_t to reduce memory usage since we only store 1s and 0s
-    int8_t *blocks;
+    // MEM_SIZE number of possible memory spots
+    int blocks[MEM_SIZE];
+    // char *strategy;
 } memory_t;
 
-// Functions for creating block of memory and allocations
-memory_t *create_memory();
+// method signatures
+memory_t *create_memory(char *memory_strategy);
 
-/*
-    Memory management strategies, returns first available index
-    or -1 if no blocks available
-*/
-int8_t first_fit(memory_t *mem, int size);
-int8_t paged_fit(memory_t *mem, int size);
-int8_t virtual_fit(memory_t *mem, int size);
+int16_t allocate_memory(memory_t *mem, char *memory_strategy, int size);
+int16_t first_fit(memory_t *mem, int size);
+int16_t paged_fit(memory_t *mem, int size);
+int16_t virtual_fit(memory_t *mem, int size);
 
-void set_memory_strategy(char *memory_strategy);
-
-// Helper function
 void free_memory(memory_t *mem, int start, int size);
-
-// Clean up
 void destroy_memory(memory_t *mem);
+
+int get_mem_usage(memory_t *mem);
+void print_memory_process(Process *p, memory_t *mem, int sim_time);
 
 #endif
