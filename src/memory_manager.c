d93ec4154ae3191d1c90709b19454e0565e5fc23
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-04-02 19:03:01 +1100

    now works with any strategy, framework for task 3

--- src/memory_manager.c
+++ src/memory_manager.c
@@ -1,50 +1,88 @@
 #include "memory_manager.h"
 
-// TODO: set memory strategies
-void set_memory_strategy(char *memory_strategy) {
-    if (strcmp(memory_strategy, "infinite") == 0) {
-        // set strategy
-    } else if (strcmp(memory_strategy, "first-fit") == 0) {
-        // set strategy
-    } else if (strcmp(memory_strategy, "paged") == 0) {
-        // set strategy
-    } else if (strcmp(memory_strategy, "virtual") == 0) {
-        // set strategy
-    } else {
-        fprintf(stderr, "Invalid memory strategy\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
+// TODO: return void* for memory, unknown memory struct type
 // create block of memory
-memory_t *create_memory(char *memory_strategy) {
-    if (strcmp(memory_strategy, "infinite") == 0) {
-        return NULL;
-    } else if (strcmp(memory_strategy, "first-fit") == 0) {
-        memory_t *mem = (memory_t *)malloc(sizeof(memory_t));
-        mem->size = MEM_SIZE;
-        mem->available = MEM_SIZE;
-        for (int i = 0; i < MEM_SIZE; i++) {
-            mem->blocks[i] = 0;
-        }
-        return mem;
+void *create_memory(mem_strategy strategy) {
+    switch(strategy) {
+        case INFINITE:
+            // no memory to create
+            return NULL;
+        case FIRST_FIT:
+            // create block memory
+            return create_block_memory();
+        case PAGED:
+            // create paged memory
+            return create_paged_memory();
+        default:
+            fprintf(stderr, "Invalid memory strategy\n");
+            exit(EXIT_FAILURE);
     }
     return NULL;
 }
 
-// allocate memory
-int16_t allocate_memory(memory_t *mem, char *memory_strategy, int size) {
-    if (strcmp(memory_strategy, "infinite") == 0) {
-        return -1;
-    } else if (strcmp(memory_strategy, "first-fit") == 0) {
-        return first_fit(mem, size);
+block_memory_t *create_block_memory() {
+    block_memory_t *mem = (block_memory_t *)malloc(sizeof(block_memory_t));
+    mem->size = MEM_SIZE;
+    mem->available = MEM_SIZE;
+    for (int i = 0; i < MEM_SIZE; i++) {
+        mem->blocks[i] = 0;
     }
-    fprintf(stderr, "Invalid memory strategy\n");
-    exit(EXIT_FAILURE);
+    return mem;
+}
+
+paged_memory_t *create_paged_memory() {
+    paged_memory_t *mem = (paged_memory_t *)malloc(sizeof(paged_memory_t));
+    mem->size = MEM_SIZE;
+    mem->available = MEM_SIZE;
+    for (int i = 0; i < NUM_PAGES; i++) {
+        mem->pages[i].frame_num = -1;
+        mem->pages[i].process_id = NULL;
+        mem->pages[i].is_allocated = false;
+        mem->frames[i].is_allocated = false;
+    }
+    return mem;
+}
+
+bool attempt_allocation(Process **p, void *mem, mem_strategy strategy) {
+    if (mem == NULL)
+        return true;
+    switch(strategy) {
+        case INFINITE: 
+            // don't need to allocate memory
+            return true;
+        case FIRST_FIT:
+            return first_fit_allocation(p, (block_memory_t *)mem);
+        case PAGED:
+            // TODO: implement paged memory
+            return true;
+        default:
+            fprintf(stderr, "Invalid memory strategy\n");
+            exit(EXIT_FAILURE);
+    }
+    // default to true
+    return true;
+}
+
+bool first_fit_allocation(Process **p, block_memory_t *mem) {
+    if ((*p)->addr == -1) {
+        // if memory available, allocate memory
+        if (mem->available >= (*p)->mem) {
+            (*p)->addr = first_fit(mem, (*p)->mem);
+            return true;
+        }
+        // if memory not available, continue to next iteration
+        else {
+            return false;
+        }
+    }
+    // memory already allocated
+    else {
+        return true;
+    }
 }
 
 // implement first fit strategy
-int16_t first_fit(memory_t *mem, int size) {
+int16_t first_fit(block_memory_t *mem, int size) {
     int start = -1;
     int count = 0;
     for (int i = 0; i < mem->size; i++) {
@@ -69,27 +107,63 @@ int16_t first_fit(memory_t *mem, int size) {
 }
 
 // free memory
-void free_memory(memory_t *mem, int start, int size) {
+void free_memory(void *mem, mem_strategy strategy, int start, int size) {
     if (mem != NULL) {
-        for (int i = start; i < start + size; i++) {
-            mem->blocks[i] = 0;
+        switch(strategy) {
+            case INFINITE:
+                // no memory to free
+                break;
+            case FIRST_FIT:
+                free_block_memory((block_memory_t *)mem, start, size);
+                break;
+            case PAGED:
+                // TODO: implement paged memory
+                break;
+            default:
+                fprintf(stderr, "Invalid memory strategy\n");
+                exit(EXIT_FAILURE);
         }
-        mem->available += size;
     }
 }
 
+void free_block_memory(block_memory_t *mem, int start, int size) {
+    for (int i = start; i < start + size; i++) {
+        mem->blocks[i] = 0;
+    }
+    mem->available += size;
+}
+
 // clean up
-void destroy_memory(memory_t *mem) {
-    if (mem != NULL)
-        free(mem);
+void destroy_memory(void *mem, mem_strategy strategy) {
+    if (mem != NULL) {
+        switch(strategy) {
+            case INFINITE:
+                // no memory to destroy
+                break;
+            case FIRST_FIT:
+                destroy_block_memory((block_memory_t *)mem);
+                break;
+            case PAGED:
+                destroy_paged_memory((paged_memory_t *)mem);
+                break;
+            default:
+                fprintf(stderr, "Invalid memory strategy\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void destroy_block_memory(block_memory_t *mem) {
+    free(mem);
+}
+
+void destroy_paged_memory(paged_memory_t *mem) {
+    // TODO: implement paged memory
+    free(mem);
 }
 
 // print memory usage
-int get_mem_usage(memory_t *mem) {
-    return ceil(100 - (((float)mem->available / (float)MEM_SIZE) * 100));
-}
-
-void print_memory_process(Process *p, memory_t *mem, int sim_time) {
+void print_memory_process(Process *p, block_memory_t *mem, int sim_time) {
     if (mem == NULL) {
         fprintf(stderr, "mem is NULL\n");
         exit(EXIT_FAILURE);
@@ -98,4 +172,8 @@ void print_memory_process(Process *p, memory_t *mem, int sim_time) {
         "%d,%s,process-name=%s,remaining-time=%d,mem-usage=%d%%,allocated-at=%"
         "d\n",
         sim_time, get_state(p), p->name, p->rtime, get_mem_usage(mem), p->addr);
+}
+
+int get_mem_usage(block_memory_t *mem) {
+    return ceil(100 - (((float)mem->available / (float)MEM_SIZE) * 100));
 }
--- src/memory_manager.h
+++ src/memory_manager.h
@@ -1,30 +1,58 @@
 #ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
-#include "process.h"
+#include "queue.h"
+#include "read.h"
 #include <math.h>
 
 #define MEM_SIZE 2048 // KB
+#define PAGE_SIZE 4   // KB
+#define NUM_PAGES 512 // 2048 / 4
 
-typedef struct memory {
+// infinite memory struct
+typedef struct block_memory_t{
     int size;
     int available;
-    // MEM_SIZE number of possible memory spots
     int blocks[MEM_SIZE];
-    // char *strategy;
-} memory_t;
+} block_memory_t;
+
+// paged memory struct
+typedef struct page_t{
+    int frame_num;
+    char* process_id;
+    bool is_allocated;
+} page_t;
+
+typedef struct frame_t{
+    bool is_allocated;
+} frame_t;
+
+typedef struct paged_memory_t{
+    int size;
+    int available;
+    page_t pages[NUM_PAGES];
+    frame_t frames[NUM_PAGES];
+} paged_memory_t;
 
 // method signatures
-memory_t *create_memory(char *memory_strategy);
+void *create_memory(mem_strategy strategy);
+block_memory_t *create_block_memory();
+paged_memory_t *create_paged_memory();
 
-int16_t allocate_memory(memory_t *mem, char *memory_strategy, int size);
-int16_t first_fit(memory_t *mem, int size);
-int16_t paged_fit(memory_t *mem, int size);
-int16_t virtual_fit(memory_t *mem, int size);
+bool attempt_allocation(Process **p, void *mem, mem_strategy strategy);
+bool first_fit_allocation(Process **p, block_memory_t *mem);
 
-void free_memory(memory_t *mem, int start, int size);
-void destroy_memory(memory_t *mem);
+int16_t first_fit(block_memory_t *mem, int size);
+int16_t paged_fit(paged_memory_t *mem, int size);
+int16_t virtual_fit(block_memory_t *mem, int size);
 
-int get_mem_usage(memory_t *mem);
-void print_memory_process(Process *p, memory_t *mem, int sim_time);
+void free_memory(void *mem, mem_strategy strategy, int start, int size);
+void free_block_memory(block_memory_t *mem, int start, int size);
+
+void destroy_memory(void *mem, mem_strategy strategy);
+void destroy_block_memory(block_memory_t *mem);
+void destroy_paged_memory(paged_memory_t *mem);
+
+void print_memory_process(Process *p, block_memory_t *mem, int sim_time);
+int get_mem_usage(block_memory_t *mem);
 
 #endif
