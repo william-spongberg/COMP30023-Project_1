9b31b9e5df3534fad7b0cdf5f5b80276e5108044
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-04-03 14:32:32 +1100

    added paged memory, TODO: fix evictions

--- src/memory_manager.c
+++ src/memory_manager.c
@@ -3,19 +3,19 @@
 // TODO: return void* for memory, unknown memory struct type
 // create block of memory
 void *create_memory(mem_strategy strategy) {
-    switch(strategy) {
-        case INFINITE:
-            // no memory to create
-            return NULL;
-        case FIRST_FIT:
-            // create block memory
-            return create_block_memory();
-        case PAGED:
-            // create paged memory
-            return create_paged_memory();
-        default:
-            fprintf(stderr, "Invalid memory strategy\n");
-            exit(EXIT_FAILURE);
+    switch (strategy) {
+    case INFINITE:
+        // no memory to create
+        return NULL;
+    case FIRST_FIT:
+        // create block memory
+        return create_block_memory();
+    case PAGED:
+        // create paged memory
+        return create_paged_memory();
+    default:
+        fprintf(stderr, "Invalid memory strategy\n");
+        exit(EXIT_FAILURE);
     }
     return NULL;
 }
@@ -23,39 +23,40 @@ void *create_memory(mem_strategy strategy) {
 block_memory_t *create_block_memory() {
     block_memory_t *mem = (block_memory_t *)malloc(sizeof(block_memory_t));
     mem->size = MEM_SIZE;
-    mem->available = MEM_SIZE;
-    for (int i = 0; i < MEM_SIZE; i++) {
+    mem->available = mem->size;
+    for (int i = 0; i < mem->available; i++) {
         mem->blocks[i] = 0;
     }
     return mem;
 }
 
-// FIXME: Does this function create the physical memory or the page table for the process? Assume this is physical memory, only the frame number is needed
+// FIXME: Does this function create the physical memory or the page table for
+// the process? Assume this is physical memory, only the frame number is needed
 paged_memory_t *create_paged_memory() {
     paged_memory_t *mem = (paged_memory_t *)malloc(sizeof(paged_memory_t));
     mem->size = MEM_SIZE;
-    mem->avail_page_count = MEM_SIZE;
-    for (int i = 0; i < NUM_PAGES; i++) {
+    mem->frames_available = NUM_PAGES;
+    for (int i = 0; i < mem->frames_available; i++) {
         mem->frames[i].is_allocated = false;
     }
+    mem->lru = NULL;
     return mem;
 }
 
-bool attempt_allocation(Process **p, void *mem, mem_strategy strategy) {
+bool attempt_allocation(Process **p, void **mem, mem_strategy strategy) {
     if (mem == NULL)
         return true;
-    switch(strategy) {
-        case INFINITE: 
-            // don't need to allocate memory
-            return true;
-        case FIRST_FIT:
-            return first_fit_allocation(p, (block_memory_t *)mem);
-        case PAGED:
-            // TODO: implement paged memory
-            return true;
-        default:
-            fprintf(stderr, "Invalid memory strategy\n");
-            exit(EXIT_FAILURE);
+    switch (strategy) {
+    case INFINITE:
+        // don't need to allocate memory
+        return true;
+    case FIRST_FIT:
+        return first_fit_allocation(p, *((block_memory_t **)mem));
+    case PAGED:
+        return paged_allocation(p, (paged_memory_t **)mem);
+    default:
+        fprintf(stderr, "Invalid memory strategy\n");
+        exit(EXIT_FAILURE);
     }
     // default to true
     return true;
@@ -77,25 +78,6 @@ bool first_fit_allocation(Process **p, block_memory_t *mem) {
     else {
         return true;
     }
-}
-
-bool paged_allocation(Process *p, paged_memory_t *mem) {
-    // FIXME: Is this check necessary? Since the process will only call this function if it has not been allocated memory
-    // check whether p's pages are allocated in memory
-        // if allocated, return true
-        // if not allocated
-            // if memory available, allocate memory, return true
-                // FIXME: Update page table in process struct and update paged_memory_t struct
-            // if memory not available
-                // FIXME: according to task3 description, it should not evict the tails of the queue but rather the least recently accessed pages
-                // FIXME: the queue is not an input to this function, so eviction cannot be done here
-                // suggestion: return false and handle eviction in the process_manager.c
-                // evict tails of queue until enough free frames
-                // FIXME: once the process manager evicts the least recently accessed pages, it should call this function again to allocate memory
-                // allocate memory, return true
-    // return false (default)
-
-
 }
 
 // implement first fit strategy
@@ -123,22 +105,106 @@ int16_t first_fit(block_memory_t *mem, int size) {
     return -1;
 }
 
+bool paged_allocation(Process **p, paged_memory_t **mem) {
+    // FIXME: Is this check necessary? Since the process will only call this
+    // function if it has not been allocated memory check whether p's pages are
+    // allocated in memory if allocated, return true if not allocated if memory
+    // available, allocate memory, return true
+    // FIXME: Update page table in process struct and update paged_memory_t
+    // struct
+    // if memory not available
+    // FIXME: according to task3 description, it should not evict the tails of
+    // the queue but rather the least recently accessed pages
+    // FIXME: the queue is not an input to this function, so eviction cannot be
+    // done here suggestion: return false and handle eviction in the
+    // process_manager.c evict tails of queue until enough free frames
+    // FIXME: once the process manager evicts the least recently accessed pages,
+    // it should call this function again to allocate memory allocate memory,
+    // return true
+    // return false (default)
+
+    if ((*p)->pages == NULL) {
+        create_pages(p);
+    }
+
+    // if pages are allocated, return true
+    bool allocated = true;
+    for (int i = 0; i < (*p)->mem / PAGE_SIZE; i++) {
+        if (!((*p)->pages[i].is_allocated)) {
+            allocated = false;
+        }
+    }
+    if (allocated) {
+        return true;
+    }
+
+    // if pages not allocated, allocate frames
+    if ((*mem)->frames_available >= (*p)->mem / PAGE_SIZE) {
+        paged_fit(p, mem);
+        return true;
+    } else {
+        // evict least recently used pages until enough free frames
+        Page *tail = NULL;
+        while ((*mem)->frames_available < (*p)->mem / PAGE_SIZE) {
+            tail = get_tail(&(*mem)->lru);
+            (*mem)->frames[tail->p->frame_num].is_allocated = false;
+            (*mem)->frames_available++;
+            delete_page(&(*mem)->lru, tail);
+        }
+        // now do paged fit
+        paged_fit(p, mem);
+        return true;
+    }
+    return false;
+}
+
+void create_pages(Process **p) {
+    (*p)->pages = (page_t *)malloc(sizeof(page_t) * ((*p)->mem / PAGE_SIZE));
+    for (int i = 0; i < (*p)->mem / PAGE_SIZE; i++) {
+        (*p)->pages[i].frame_num = -1;
+        (*p)->pages[i].is_allocated = false;
+    }
+}
+
+// implement paged fit strategy
+void paged_fit(Process **p, paged_memory_t **mem) {
+    page_t *page = NULL;
+    for (int i = 0; i < (*p)->mem / PAGE_SIZE; i++) {
+        page = &(*p)->pages[i];
+        insert_page(&(*mem)->lru, page);
+    }
+
+    int i = 0;
+    int j = 0;
+    while (i < (*mem)->size && j < (*p)->mem / PAGE_SIZE) {
+        if (!(*mem)->frames[i].is_allocated) {
+            (*p)->pages[j].frame_num = i;
+            (*p)->pages[j].is_allocated = true;
+            (*mem)->frames[i].is_allocated = true;
+            (*mem)->frames_available--;
+            j++;
+        }
+        i++;
+    }
+}
+
 // free memory
-void free_memory(void *mem, mem_strategy strategy, int start, int size) {
+void free_memory(void **mem, Process **p, mem_strategy strategy, int start,
+                 int size) {
     if (mem != NULL) {
-        switch(strategy) {
-            case INFINITE:
-                // no memory to free
-                break;
-            case FIRST_FIT:
-                free_block_memory((block_memory_t *)mem, start, size);
-                break;
-            case PAGED:
-                // TODO: implement paged memory
-                break;
-            default:
-                fprintf(stderr, "Invalid memory strategy\n");
-                exit(EXIT_FAILURE);
+        switch (strategy) {
+        case INFINITE:
+            // no memory to free
+            break;
+        case FIRST_FIT:
+            free_block_memory(*(block_memory_t **)mem, start, size);
+            break;
+        case PAGED:
+            free_paged_memory((paged_memory_t **)mem, *p);
+            break;
+        default:
+            fprintf(stderr, "Invalid memory strategy\n");
+            exit(EXIT_FAILURE);
         }
     }
 }
@@ -150,22 +216,30 @@ void free_block_memory(block_memory_t *mem, int start, int size) {
     mem->available += size;
 }
 
+void free_paged_memory(paged_memory_t **mem, Process *p) {
+    for (int i = 0; i < p->mem / PAGE_SIZE; i++) {
+        (*mem)->frames[p->pages[i].frame_num].is_allocated = false;
+        (*mem)->frames_available++;
+        delete_page(&(*mem)->lru, get_page(&(*mem)->lru, &p->pages[i]));
+    }
+}
+
 // clean up
 void destroy_memory(void *mem, mem_strategy strategy) {
     if (mem != NULL) {
-        switch(strategy) {
-            case INFINITE:
-                // no memory to destroy
-                break;
-            case FIRST_FIT:
-                destroy_block_memory((block_memory_t *)mem);
-                break;
-            case PAGED:
-                destroy_paged_memory((paged_memory_t *)mem);
-                break;
-            default:
-                fprintf(stderr, "Invalid memory strategy\n");
-                exit(EXIT_FAILURE);
+        switch (strategy) {
+        case INFINITE:
+            // no memory to destroy
+            break;
+        case FIRST_FIT:
+            destroy_block_memory((block_memory_t *)mem);
+            break;
+        case PAGED:
+            destroy_paged_memory((paged_memory_t *)mem);
+            break;
+        default:
+            fprintf(stderr, "Invalid memory strategy\n");
+            exit(EXIT_FAILURE);
         }
     }
 }
@@ -188,9 +262,36 @@ void print_memory_process(Process *p, block_memory_t *mem, int sim_time) {
     printf(
         "%d,%s,process-name=%s,remaining-time=%d,mem-usage=%d%%,allocated-at=%"
         "d\n",
-        sim_time, get_state(p), p->name, p->rtime, get_mem_usage(mem), p->addr);
+        sim_time, get_state(p), p->name, p->rtime, get_block_mem_usage(mem),
+        p->addr);
 }
 
-int get_mem_usage(block_memory_t *mem) {
-    return ceil(100 - (((float)mem->available / (float)MEM_SIZE) * 100));
+// print paged process
+void print_paged_process(Process *p, paged_memory_t *mem, int sim_time) {
+    if (mem == NULL) {
+        fprintf(stderr, "mem is NULL\n");
+        exit(EXIT_FAILURE);
+    }
+    printf(
+        "%d,%s,process-name=%s,remaining-time=%d,mem-usage=%d%%,mem-frames=[",
+        sim_time, get_state(p), p->name, p->rtime, get_paged_mem_usage(mem));
+    for (int i = 0; i < p->mem / PAGE_SIZE; i++) {
+        if (i == p->mem / PAGE_SIZE - 1) {
+            printf("%d]\n", p->pages[i].frame_num);
+        } else {
+            printf("%d,", p->pages[i].frame_num);
+        }
+    }
+}
+
+//void print_evicted_frames()
+
+int get_block_mem_usage(block_memory_t *mem) {
+    return ceil(100 - (((float)mem->available / (float)mem->size) * 100));
+}
+
+int get_paged_mem_usage(paged_memory_t *mem) {
+    return ceil(
+        100 -
+        (((float)mem->frames_available * PAGE_SIZE / (float)mem->size) * 100));
 }
--- src/memory_manager.h
+++ src/memory_manager.h
@@ -1,11 +1,11 @@
 #ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
-#include "queue.h"
+
+#include "least_recently_used.h"
 #include "read.h"
 #include <math.h>
 
 #define MEM_SIZE 2048 // KB
-#define PAGE_SIZE 4   // KB
 #define NUM_PAGES 512 // 2048 / 4
 
 // infinite memory struct
@@ -18,24 +18,17 @@ typedef struct block_memory_t{
 // TODO: potentially add page_t as array to process struct on creation
 // figure out number of pages by p->mem / PAGE_SIZE
 // or alternatively keep using paged_memory_t as a sort of MMU
-// FIXME: The page_t struct should only be used by process struct to store in page table linkedlist, and it should inlcude information about the frame it maps to
 // FIXME: Last accessed time and the process it belongs to.
-typedef struct page_t{
-    int frame_num;
-    char* process_id;
-    bool is_allocated;
-} page_t;
 
 typedef struct frame_t{
     bool is_allocated;
 } frame_t;
 
 // paged memory struct
-// FIXME: Use separate structs for physical memory(frames) and virtual memory(pages)
 typedef struct paged_memory_t{
     int size;
-    int avail_page_count;
-    // page_t pages[NUM_PAGES]; FIXME: commented out for now, this should be a linkedlist of pages in the process struct 
+    int frames_available;
+    Page *lru;
     frame_t frames[NUM_PAGES];
 } paged_memory_t;
 
@@ -44,21 +37,26 @@ void *create_memory(mem_strategy strategy);
 block_memory_t *create_block_memory();
 paged_memory_t *create_paged_memory();
 
-bool attempt_allocation(Process **p, void *mem, mem_strategy strategy);
+bool attempt_allocation(Process **p, void **mem, mem_strategy strategy);
 bool first_fit_allocation(Process **p, block_memory_t *mem);
+bool paged_allocation(Process **p, paged_memory_t **mem);
+void create_pages(Process **p);
 
 int16_t first_fit(block_memory_t *mem, int size);
-int16_t paged_fit(paged_memory_t *mem, int size);
+void paged_fit(Process **p, paged_memory_t **mem);
 int16_t virtual_fit(block_memory_t *mem, int size);
 
-void free_memory(void *mem, mem_strategy strategy, int start, int size);
+void free_memory(void **mem, Process **p, mem_strategy strategy, int start, int size);
 void free_block_memory(block_memory_t *mem, int start, int size);
+void free_paged_memory(paged_memory_t **mem, Process *p);
 
 void destroy_memory(void *mem, mem_strategy strategy);
 void destroy_block_memory(block_memory_t *mem);
 void destroy_paged_memory(paged_memory_t *mem);
 
 void print_memory_process(Process *p, block_memory_t *mem, int sim_time);
-int get_mem_usage(block_memory_t *mem);
+void print_paged_process(Process *p, paged_memory_t *mem, int sim_time);
+int get_block_mem_usage(block_memory_t *mem);
+int get_paged_mem_usage(paged_memory_t *mem);
 
 #endif
